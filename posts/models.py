0f9dcbb86553b54591024de08edcdd46fb099365
samira-ai-2020 <sami549967@gmail.com>
2023-01-14 23:02:23 +0330

    add admin and class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title= models.CharField(max_length=50)
+    text= models.TextField()
+    is_enable= models.BooleanField(default=False)
+    publish_date= models.DateField()
+    created_time= models.DateTimeField()
+    updated_time= models.DateTimeField()
+
