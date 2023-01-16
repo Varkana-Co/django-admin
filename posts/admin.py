338856f3101f710afcf8d43a8bef6768a088df9c
samira-ai-2020 <sami549967@gmail.com>
2023-01-16 23:53:49 +0330

    define new class in models, add comment

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id','title', 'is_enable', 'publish_date', 'created_time', 'updated_time']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['post', 'text_comment', 'created_time']
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
