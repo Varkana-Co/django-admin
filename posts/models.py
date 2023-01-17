a5c1a9709bc57795116e4da6c2802eded6821f5a
samira-ai-2020 <sami549967@gmail.com>
2023-01-17 13:23:44 +0330

    last change

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# post model
 class Post(models.Model):
     title= models.CharField(max_length=50)
     text= models.TextField(blank=True)
@@ -11,6 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
 
 
 
