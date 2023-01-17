a2091f80dc49b0b56d7689fbae3c67cad2d5f188
samira-ai-2020 <sami549967@gmail.com>
2023-01-17 16:33:27 +0330

    add comment in post

--- posts/admin.py
+++ posts/admin.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 
 from .models import Post, Comment
 
+
+class CommentAdminInline(admin.TabularInline):
+    model = Comment 
+    field = ['text',]
+
+
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display=['id','title', 'is_enable', 'publish_date', 'created_time', 'updated_time']
+    inlines = [CommentAdminInline, ]
 
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display=['post', 'text_comment', 'created_time']
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display=['post', 'text_comment', 'created_time']
 
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
+# admin.site.register(Comment, CommentAdmin)
