f4cd10c280bc615cf69347c7d03566563f7d4456
samira-ai-2020 <sami549967@gmail.com>
2023-01-23 09:50:09 +0330

    original samira

--- posts/forms.py
+++ posts/forms.py
@@ -4,6 +4,7 @@ from django import forms
 class PostForms(forms.Form):
     title = forms.CharField()
     text = forms.CharField(widget=forms.Textarea)
+    password = forms.CharField(widget=forms.PasswordInput)
     is_enable = forms.BooleanField()
     publish_date = forms.DateField()
 
